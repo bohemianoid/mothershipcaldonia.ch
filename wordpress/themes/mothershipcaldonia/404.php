00631cf3e7e92944d14118014c72431186460585
simonroth <git@s.imon.ch>
2015-07-27 21:46:27 +0200

    Initial commit

---
+++ wordpress/themes/mothershipcaldonia/404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Mothership Caldonia
+ */
+
+get_header(); ?>
+
+<main class="clearfix">
+  <article class="aside">
+    <h2>Seite nicht gefunden</h2>
+    <p>Die angeforderte Seite existiert nicht. Weiter zur <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Startseite</a>.</p>
+  </article>
+</section>
+</main>
+
+<?php get_footer(); ?>
