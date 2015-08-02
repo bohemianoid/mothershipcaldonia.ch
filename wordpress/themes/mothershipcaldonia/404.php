a917891cc74d14dea2777d00f64cb8149225cf1e
simonroth <git@s.imon.ch>
2015-08-02 12:38:32 +0200

    Initial commit

--- wordpress/themes/mothershipcaldonia/404.php
+++ wordpress/themes/mothershipcaldonia/404.php
@@ -10,6 +10,7 @@ get_header(); ?>
     <article>
       <h2>Seite nicht gefunden</h2>
       <p>Die angeforderte Seite existiert nicht. Weiter zur <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Startseite</a>.</p>
+      <p style="float: left;">AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz01234567890</p>
     </article>
   </div>
 </section>
--- wordpress/themes/mothershipcaldonia/header.php
+++ wordpress/themes/mothershipcaldonia/header.php
@@ -18,15 +18,19 @@
       <button class="header__button button--link" id="js-open-overlay" type="button"><span class="fa fa-angle-double-down icon-push"></span>Menu</button>
       <h1 class="header__band-name"><span class="band-name__block band-name__block--left-aligned">Mother</span><span class="band-name__block band-name__block--centered"><?php echo !is_404() ? ( !get_query_var( 'easter', false ) ? 'Ship' : '5hip' ) : '404' ?></span> <span class="band-name__block band-name__block--right-aligned">Caldonia</span></h1>
     </header>
-    <div class="overlay overlay--slidedown" id="js-overlay">
-      <button class="overlay__button button--link" id="js-close-overlay" type="button"><span class="fa fa-angle-double-up"></span><span class="visuallyhidden">Schliessen</span></button>
-      <?php wp_nav_menu( array(
-        'theme_location' => 'primary',
-        'container' => 'nav',
-        'container_class' => 'overlay__nav',
-        'menu_class' => 'nav__menu',
-        'menu_id' => 'primary'
-      ) ); ?>
+    <div class="container overlay-body--slidedown" id="js-overlay">
+      <div class="overlay__main">
+        <button class="overlay-button--close button--link" id="js-close-overlay" type="button"><span class="fa fa-angle-double-up"></span><span class="visuallyhidden">Schliessen</span></button>
+        <nav class="overlay-nav">
+          <ul>
+            <?php wp_nav_menu( array(
+              'theme_location' => 'primary',
+              'container'      => false,
+              'items_wrap'     => '%3$s'
+            ) ); ?>
+          </ul>
+        </nav>
+      </div>
       <footer class="overlay__footer">
         <ul class="social social--horizontal">
           <li><a href="#">Facebook</a></li>
