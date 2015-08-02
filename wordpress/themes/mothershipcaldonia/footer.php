0462684fd6718439f08ffa89ca092e0e76ca9620
simonroth <git@s.imon.ch>
2015-08-02 20:36:03 +0200

    Piwik added

--- wordpress/themes/mothershipcaldonia/footer.php
+++ wordpress/themes/mothershipcaldonia/footer.php
@@ -8,6 +8,21 @@
   <footer class="made-with">
     <a href="http://simonroth.ch">Entwickelt mit <span class="fa fa-heart"></span><span class="visuallyhidden"> Herz</span> in Luzern</a>
   </footer>
+  <!-- Piwik
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//piwik.mothershipcaldonia.ch/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', 1]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <noscript><p><img src="//piwik.mothershipcaldonia.ch/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+  End Piwik Code -->
   <?php wp_footer(); ?>
   </body>
 </html>
