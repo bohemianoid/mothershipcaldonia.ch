e1a39b72e7e792463ff0144264e59e7db344f418
Simon Roth <git@s.imon.ch>
2015-11-24 16:33:39 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mothershipcaldonia.ch
 Band Website of Mothership Caldonia
+
+* https://github.com/Automattic/_s
+* https://github.com/WordPress/WordPress/tree/master/wp-content/themes/twentyfifteen
+* https://github.com/toddmotto/html5blank/tree/master/src
+* https://github.com/roots/sage
+* https://github.com/eddiemachado/bones
--- wordpress/themes/mothershipcaldonia/functions.php
+++ wordpress/themes/mothershipcaldonia/functions.php
@@ -1,67 +1,20 @@
 <?php
 /**
- * @package Mothership Caldonia
+ * Mothership Caldonia functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * @link https://github.com/roots/sage/blob/master/functions.php
+ *
+ * @package Mothership_Caldonia
  */
+$mc_includes = [
+  'post-types/event.php' // Event post type
+];
 
-if ( ! function_exists( 'mc_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- */
-function mc_setup() {
-  // Let WordPress manage the document title.
-  add_theme_support( 'title-tag' );
-
-  // This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => esc_html( 'Primary Menu' )
-	) );
+foreach ($mc_includes as $file) {
+  if (!$filepath = locate_template($file)) {
+    trigger_error(sprintf(__('Error locating %s for inclusion', 'mc'), $file), E_USER_ERROR);
+  }
+  require_once $filepath;
 }
-endif; // mc_setup
-add_action( 'after_setup_theme', 'mc_setup' );
-
-/**
- * Font URLs function.
- */
-function mc_fonts_url() {
-  $font_families[] = 'Oswald:700';
-  $font_families[] = 'Open Sans:600,400';
-  $font_families[] = 'PT Mono';
-
-  $query_args = array(
-    'family' => urlencode( implode( '|', $font_families ) ),
-    'subset' => urlencode( 'latin' )
-  );
-
-  $fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
-  return esc_url_raw( $fonts_url );
-}
-
-/**
- * Enqueue scripts and styles.
- */
-function mc_scripts() {
-  wp_enqueue_style( 'mc-fonts', mc_fonts_url(), array(), null );
-
-  wp_enqueue_style( 'mc-icons', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(), null );
-
-  wp_enqueue_style( 'mc-style', get_stylesheet_uri(), array(), null );
-
-  wp_enqueue_script( 'mc-modernizr', get_template_directory_uri() . '/js/modernizr.min.js', array(), null, true );
-
-  wp_enqueue_script( 'mc-scripts', get_template_directory_uri() . '/js/scripts.min.js', array(), null, true );
-}
-add_action( 'wp_enqueue_scripts', 'mc_scripts' );
-
-/*
- * Easter egg
- */
- function mc_easter_egg_route() {
-   add_rewrite_rule( '^5', 'index.php?easter=egg', 'top' );
- }
- add_action( 'init', 'mc_easter_egg_route' );
-
- function mc_easter_egg_var( $vars ) {
-   $vars[] = 'easter';
-   return $vars;
- }
- add_filter( 'query_vars', 'mc_easter_egg_var' );
+unset($file, $filepath);
